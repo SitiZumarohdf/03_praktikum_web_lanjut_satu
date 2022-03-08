f72466971f15ea3e273f0539be17e11d3f5c7e46
= <=>
2022-03-08 22:36:31 +0700

    Js3 prakk1

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
